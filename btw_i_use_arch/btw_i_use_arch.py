33474ca9f137fcf625339be14ff4b001874f4a0e
Justin <kellerjustin@protonmail.com>
2021-06-14 09:46:11 -0500

    adds powerlevel10k plugin for zsh

--- btw_i_use_arch/btw_i_use_arch.py
+++ btw_i_use_arch/btw_i_use_arch.py
@@ -106,6 +106,7 @@ class Install:
             'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
             "git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions",
             "git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting",
+            "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k",
         ]
         for command in commands:
             subprocess.run(command, shell=True)
