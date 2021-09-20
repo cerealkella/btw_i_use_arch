9f4704246dc60473d8f2838aa11213fac0e19724
Justin <kellerjustin@protonmail.com>
2021-09-20 17:11:22 -0500

    fixed simplehelp alias

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -9,3 +9,7 @@ alias rdp="xfreerdp /v:$RDP_HOST /u:$DOMAIN_USERNAME /d:$DOMAIN /p:(bw get passw
 
 # Bitwarden setup
 alias wardenmybits="python $HOME/btw_i_use_arch/btw_i_use_arch/warden.py"
+
+# SimpleHelp App
+alias simplehelp_app="'$HOME/.JWrapper/JWrapper-SimpleHelp Technician/SimpleHelp TechnicianLinLauncher64' JWVAPP SimpleHelp_Technician"
+alias simplehelp="bw get password 'Simplehelp Admin' | xclip -sel clip && simplehelp_app"
