8bd2bfaeecbb4ae18398cf2e13ec855b5412e18e
Justin <kellerjustin@protonmail.com>
2021-09-20 23:07:45 -0500

    adds jq and bw examples

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -13,3 +13,10 @@ alias wardenmybits="python $HOME/btw_i_use_arch/btw_i_use_arch/warden.py"
 # SimpleHelp App
 alias simplehelp_app="'$HOME/.JWrapper/JWrapper-SimpleHelp Technician/SimpleHelp TechnicianLinLauncher64' JWVAPP SimpleHelp_Technician"
 alias simplehelp="bw get password 'Simplehelp Admin' | xclip -sel clip && simplehelp_app"
+
+# GitHub SSH Passphrase to clipboard Example (laptop)
+# example to find id:
+# bw list items --search github | jq ".[0].id"
+alias github_passphrase="bw get item '89c604f4-a57b-4df4-882f-a9bc014e7c96' | jq '.fields[0].value' | xclip -sel clip"
+# GitLab
+alias gitlab_passphrase="bw get item 'f1f1ca3f-135d-426c-8250-a9bc014e7c96' | jq '.fields[0].value' | xclip -sel clip"
