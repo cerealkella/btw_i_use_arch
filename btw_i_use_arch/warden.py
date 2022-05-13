b7974b4478c4d3dd6003a29fd2b3f95506301dd8
Justin <kellerjustin@protonmail.com>
2022-05-13 08:58:17 -0500

    making the warden stuff actually work

--- btw_i_use_arch/warden.py
+++ btw_i_use_arch/warden.py
@@ -1,29 +1,60 @@
 import os
+import sys
 from pathlib import PosixPath
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, run
 
 
-def set_bw_session():
-    """Sets the BW_SESSION Environmental variable via the zsh custom file"""
-    bw_session = os.getenv("BW_SESSION")
-    omz_env = PosixPath("~/").expanduser().joinpath(".oh-my-zsh/custom/2_environment.zsh")
-
-    if bw_session is None or bw_session == "":
-
-        command = "bw unlock --raw"
-        with Popen(command, stdout=PIPE, stderr=None, shell=True) as process:
-            bw_session = process.communicate()[0].decode("utf-8")
-
-    with open(omz_env, "w") as file_object:
-        file_object.write(f'export BW_SESSION="{bw_session}"')
-    return bw_session
-
-
-def unset_bw_session():
-    """Unsets the BW_SESSION Environment variable"""
-    command = "unset BW_SESSION"
+def run_command(command):
+    """Runs a thing in the shell"""
     with Popen(command, stdout=PIPE, stderr=None, shell=True) as process:
-        process.communicate()[0].decode("utf-8")
+        return process.communicate()[0].decode("utf-8")
 
 
-set_bw_session()
+class WardenMyBits:
+    zsh_file = "00_bitwarden.zsh"
+    tempfile = PosixPath("/tmp/").joinpath(zsh_file)
+    omz_symlink = (
+        PosixPath("~/").expanduser().joinpath(".oh-my-zsh/custom").joinpath(zsh_file)
+    )
+
+    def symlink_valid(self):
+        """Determines if bitwarden temp file exists and has a valid symlink"""
+        if self.tempfile.is_file() and self.omz_symlink.is_file():
+            return self.omz_symlink.resolve() == self.tempfile
+        else:
+            return False
+
+    def remove_symlink(self, force=False):
+        """Removes symlink if broken, pass force=True to force removal of symlink"""
+        if not self.symlink_valid() or force:
+            if self.omz_symlink.is_file():
+                self.omz_symlink.unlink()
+            run_command("unset BW_SESSION")
+        else:
+            print("Valid symlink, not removing!")
+
+    def set_bw_session(self):
+        """Sets the BW_SESSION Environment variable via the zsh custom file"""
+        print("Setting BW_SESSION files & variables...")
+        self.remove_symlink(force=True)
+        command = ["bw", "login", "--check"]
+        output = run(command, capture_output=True)
+        if output.stderr.decode("utf-8") == "You are not logged in.":
+            print("Logging in...")
+            bw_session = run_command("bw login --raw")
+        else:
+            print("Logged in, unlocking BitWarden session...")
+            bw_session = run_command("bw unlock --raw")
+        with open(self.tempfile, "w") as file_object:
+            file_object.write(f'export BW_SESSION="{bw_session}"')
+        os.symlink("/tmp/00_bitwarden.zsh", self.omz_symlink)
+        return bw_session
+
+
+def main():
+    warden = WardenMyBits()
+    warden.set_bw_session()
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
