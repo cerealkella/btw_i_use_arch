a10e270af5509e42ae40bc7ec419e28794c16a75
Justin <kellerjustin@protonmail.com>
2023-10-30 08:34:10 -0500

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kellerjustin/btw_i_use_arch",
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
 )
